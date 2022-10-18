1eb8e8a708d0225cc072a9583651d024905e9bb3
AshiX-0 <ashix_sama@outlook.com>
2022-10-18 11:27:50 +0200

    Completed database table editing functions

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 
 mongoose.connect(process.env.URI, {useNewUrlParser:true, useUnifiedTopology:true})
     .then(async ()=>{
-        console.log('Successful connection to database');        
+        console.log('Successful connection to database');  
+        //findLocationID("63485052d3e14c8cd7b95b5e");
+        //findLocationsByFilmName("Ca ne va pas Supermarché");
+        //deleteLocationByID("63485052d3e14c8cd7b95b5e");
+        //updateLocationID('63485052d3e14c8cd7b95b5f',{filmDirectorName:"Sandrine Kiberlaine"});
         //const dataImport = await ImportFile();
         mongoose.connection.close();
     })
@@ -35,3 +39,56 @@ async function ImportFile() {
     })
     return res;
 }
+
+async function findLocationID(_id){
+    Location.findById({_id}).then((res)=>{
+        console.log(res);
+        return res;
+    }).catch((err)=>{
+        console.error("Error in querying", err.message);
+        return err;
+    })
+}
+
+async function findLocationsByFilmName(fName){
+    Location.find({filmName:fName}).then((res)=> {
+        console.log(res);
+        return res;
+    }).catch((err)=>{
+        console.error("Error in querying", err.message);
+    });
+}
+
+async function deleteLocationByID(_id){
+    Location.deleteOne({_id:_id}).then((res)=>{
+        console.log(res);
+        return res;
+    }).catch((err)=>{
+        console.error("Error in querying", err.message);
+        return err;
+    })
+}
+async function updateLocationID(_id, updtateObject){
+    Location.findByIdAndUpdate(_id,updtateObject).then((res)=>{
+        console.log(res);
+        return res;
+    }).catch((err)=>{
+        console.error("Error in querying", err.message);
+        return err;
+    })
+}
+
+async function createLocation(type_tournage,nom_producteur,date_fin,nom_tournage,ardt_lieu,geo_shape,id_lieu,nom_realisateur,adresse_lieu,date_debut,annee_tournage){    
+    const ref_date_fin = new Date(date_fin)
+    const ref_date_debut = new Date(date_debut)
+    const newEle = new Location({filmType: type_tournage,filmProducerName: nom_producteur,endDate:ref_date_fin,filmName:nom_tournage,district:ardt_lieu,geolocalisation:geo_shape,sourceLocationId:id_lieu,filmDirectorName:nom_realisateur,address:adresse_lieu,startDate:ref_date_debut,year:annee_tournage});
+    newEle.save().then((res)=> {
+        console.log(res);
+        return res;
+    }).catch((err)=>{
+        console.error("Error during save", err.message);
+        return err;
+    });
+}
+
+
